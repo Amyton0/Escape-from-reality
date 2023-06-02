4309f3fb9a2e11cdaf1ba969f0933af33c57f5d7
p-om-idor <115640053+p-om-idor@users.noreply.github.com>
2023-06-02 06:05:59 +0500

    ВНИМАНИЕ АНЕКДОТ

--- EFR/Assets/Code/MainMenu.cs
+++ EFR/Assets/Code/MainMenu.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
     public void PlayGame()
     {
-        SceneManager.LoadScene("Assets/Scenes/Lobby.unity");
+        SceneManager.LoadScene(1);
     }
 
     public void ExitGame()
     {
-        Debug.Log("Игра закрылась");
         Application.Quit();
     }
 }
